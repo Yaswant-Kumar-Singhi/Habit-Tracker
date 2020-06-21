eacda5e836f990ed30ad6a5f8161fcb4f509a2e0
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2020-06-21 21:57:26 +0530

    setting up mongo store

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 const saasMiddleware = require('node-sass-middleware');
 const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 
 
 
@@ -9,6 +10,7 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo')(session);
 
 
 const port = 8000;
@@ -59,7 +61,16 @@ app.use(session({
     resave : false,
     cookie : {
         maxAge : (1000 * 60 * 100)
-    }
+    },
+    store : new MongoStore(
+        {
+        
+            mongooseConnection : mongoose.connection,
+            autoRemove : 'disabled'
+
+        }, function(err){
+        console.log(err)
+    })
 }));
 
 app.use(passport.initialize());
