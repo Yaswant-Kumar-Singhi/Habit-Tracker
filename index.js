7c948cc4d464a4d2715fc67ffc80e8e229d2e939
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2020-06-21 14:04:20 +0530

    creating express server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const port = 8000;
+
+const app = express();
+
+
+
+app.listen(port,function(err,data){
+    if(err){
+        console.log("Error in running the server on Listening ", err);
+        return ;
+    }
+    console.log(`Succcussfully connected to ${port}`);
+});
